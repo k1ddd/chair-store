2029396f4499be6c8fac37193f96e3d6ff936b61
vladyslav <vladyslav.klokun@gmail.com>
2024-01-24 22:24:49 +0200

    add some color hint

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const infoBtns = document.querySelectorAll(".info-dot");
+const infoHints = document.querySelectorAll(".info-hint");
+
+for (let btn of infoBtns) {
+  btn.addEventListener("click", function (ev) {
+    ev.stopPropagation();
+    this.parentNode.querySelector(".info-hint").classList.toggle("none");
+  });
+}
+
+document.addEventListener("click", function () {
+  for (let hint of infoHints) {
+    hint.classList.add("none");
+  }
+});
+
+for (let hint of infoHints) {
+  hint.addEventListener("click", (ev) => ev.stopPropagation());
+}
