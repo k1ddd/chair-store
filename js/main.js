e6cc73d0329fab2deb9d2a6d6259f4175ccaec2e
vladyslav <vladyslav.klokun@gmail.com>
2024-05-10 23:40:47 +0300

    connected swiper

--- js/main.js
+++ js/main.js
@@ -17,3 +17,36 @@ document.addEventListener("click", function () {
 for (let hint of infoHints) {
   hint.addEventListener("click", (ev) => ev.stopPropagation());
 }
+
+// Swiper slider //
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBetween: 42,
+  freeMode: true,
+  loop: true,
+
+  // Breakpoints //
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 40,
+  //   },
+  //   1024: {
+  //     slidesPerView: 5,
+  //     spaceBetween: 50,
+  //   },
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#sliderNext",
+    prevEl: "#sliderPrev",
+  },
+});
